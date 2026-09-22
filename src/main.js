14f6a588d24e8f1608e64313b9e8d30a2790edcc
KNJ48 <kenjiiin421@gmail.com>
2026-09-22 20:43:39 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import * as THREE from "three";
 
+import {
+  WORLD_MAP
+} from "./worldMap.js";
+
 // ==================================================
 // WORLD SCALE
 // ==================================================
